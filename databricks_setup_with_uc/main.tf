25cdd819186d985ee852edffac3536feaf081463
arulrajgopal <arulrajgopal@oulook.com>
2025-12-07 18:02:21 +0530

    update

--- databricks_setup_with_uc/main.tf
+++ databricks_setup_with_uc/main.tf
@@ -8,44 +8,112 @@ provider "azurerm" {
   client_secret   = var.client_secret
 }
 
-# resource group and storage account
-
-resource "azurerm_resource_group" "resource_group" {
-  name     = "test_resource_group"
-  location = "South India"
+terraform {
+  required_providers {
+    azurerm = {
+      source  = "hashicorp/azurerm"
+      version = "~> 4.0"
+    }
+  }
 }
 
-resource "azurerm_storage_account" "storage_account_dev" {
-  name                     = "kaniniproadlsdev"
-  resource_group_name       = azurerm_resource_group.resource_group.name
-  location                 = azurerm_resource_group.resource_group.location
-  account_tier              = "Standard"
+# Create Resource Group
+resource "azurerm_resource_group" "this" {
+  name     = var.resource_group_name
+  location = var.location
+}
+
+# Create Storage Account
+resource "azurerm_storage_account" "this" {
+  name                     = var.storage_account_name
+  resource_group_name      = azurerm_resource_group.this.name
+  location                 = azurerm_resource_group.this.location
+  account_tier             = "Standard"
   account_replication_type = "LRS"
-  is_hns_enabled           = true
+  
+  tags = {
+    environment = var.environment
+  }
+}
+
+# Create Storage Container
+resource "azurerm_storage_container" "this" {
+  name                  = var.storage_container_name
+  storage_account_id    = azurerm_storage_account.this.id
+  container_access_type = "private"
+}
+
+# Create Databricks Access Connector
+resource "azurerm_databricks_access_connector" "this" {
+  name                = var.access_connector_name
+  resource_group_name = azurerm_resource_group.this.name
+  location            = azurerm_resource_group.this.location
+  
+  identity {
+    type = "SystemAssigned"
+  }
 
   tags = {
-    environment = "Terraform"
+    environment = var.environment
   }
 }
 
-
-# databricks workspace
-
-resource "azurerm_resource_group" "rg_dev" {
-  name     = "dbx-dev"
-  location = "South India"
-}
-
-
-resource "azurerm_databricks_workspace" "dbx1" {
-  name                = "kaninipro-dbx1-dev"
-  resource_group_name = azurerm_resource_group.rg_dev.name
-  location            = azurerm_resource_group.rg_dev.location
-  sku                 = "premium"
-
-    tags = {
-    environment = "Terraform"
+# Create Databricks Workspace
+resource "azurerm_databricks_workspace" "this" {
+  name                = var.workspace_name
+  resource_group_name = azurerm_resource_group.this.name
+  location            = azurerm_resource_group.this.location
+  sku                 = var.databricks_sku
+  
+  tags = {
+    environment = var.environment
   }
+
+  depends_on = [
+    azurerm_role_assignment.access_connector_storage_blob_contributor
+  ]
 }
 
+# Grant Storage Blob Data Contributor access to Access Connector
+resource "azurerm_role_assignment" "access_connector_storage_blob_contributor" {
+  scope              = azurerm_storage_account.this.id
+  role_definition_name = "Storage Blob Data Contributor"
+  principal_id       = azurerm_databricks_access_connector.this.identity[0].principal_id
+}
+
+# Output values
+output "resource_group_id" {
+  value       = azurerm_resource_group.this.id
+  description = "The ID of the created resource group"
+}
+
+output "storage_account_id" {
+  value       = azurerm_storage_account.this.id
+  description = "The ID of the created storage account"
+}
+
+output "storage_account_name" {
+  value       = azurerm_storage_account.this.name
+  description = "The name of the created storage account"
+}
+
+output "databricks_workspace_id" {
+  value       = azurerm_databricks_workspace.this.id
+  description = "The ID of the created Databricks workspace"
+}
+
+output "databricks_workspace_url" {
+  value       = azurerm_databricks_workspace.this.workspace_url
+  description = "The Databricks workspace URL"
+}
+
+output "access_connector_id" {
+  value       = azurerm_databricks_access_connector.this.id
+  description = "The ID of the Access Connector"
+}
+
+output "access_connector_principal_id" {
+  value       = azurerm_databricks_access_connector.this.identity[0].principal_id
+  description = "The principal ID of the Access Connector managed identity"
+}
 
--- databricks_setup_with_uc/variables.tf
+++ databricks_setup_with_uc/variables.tf
@@ -17,3 +17,52 @@ variable "client_secret" {
   type    = string
   default = "XXXX"
 }
+
+variable "location" {
+  type        = string
+  description = "Azure region where resources will be created"
+  default     = "East US"
+}
+
+variable "resource_group_name" {
+  type        = string
+  description = "Name of the resource group"
+  default     = "rg-databricks"
+}
+
+variable "environment" {
+  type        = string
+  description = "Environment name"
+  default     = "dev"
+}
+
+variable "storage_account_name" {
+  type        = string
+  description = "Name of the storage account (must be globally unique, lowercase, 3-24 characters)"
+  default     = "kaniniprostorageaccountdev"
+}
+
+variable "storage_container_name" {
+  type        = string
+  description = "Name of the storage container"
+  default     = "databricks-container"
+}
+
+variable "access_connector_name" {
+  type        = string
+  description = "Name of the Databricks access connector"
+  default     = "databricks-connector"
+}
+
+variable "workspace_name" {
+  type        = string
+  description = "Name of the Databricks workspace"
+  default     = "databricks-workspace"
+}
+
+variable "databricks_sku" {
+  type        = string
+  description = "SKU of the Databricks workspace (standard, premium, or trial)"
+  default     = "premium"
+}
+
