b5ccdce48329907c0db316713a4c5cda58dad334
arulrajgopal <arulrajgopal@oulook.com>
2025-12-27 16:51:29 +0530

    update

--- databricks_SDP_schema_drift/src/databricks_dlt_schema_drift_etl/transformations/orders_customers.py
+++ databricks_SDP_schema_drift/src/databricks_dlt_schema_drift_etl/transformations/orders_customers.py
@@ -1,41 +1,78 @@
 from pyspark import pipelines as dp
-from pyspark.sql.functions import current_timestamp, row_number, col
-from pyspark.sql.window import Window
-from pyspark.sql.types import *
+from pyspark.sql.functions import current_timestamp, col
 
 
-# source lookup table
+catalog = "kaninipro_catalog"
+database = "dev"
+catalog_database = f"{catalog}.{database}"
+volumn_path = "/Volumes/kaninipro_catalog/dev/landing_zone/"
+
+dp.create_streaming_table("raw_customers", comment="Incremental load")
+
+@dp.append_flow(
+  target = "raw_customers"
+)
+def raw_customers():
+  return (
+      spark.readStream
+          .format("cloudFiles")
+          .option("cloudFiles.format", "parquet")
+          .option("cloudFiles.inferColumnTypes", "true")
+          .load(f"{volumn_path}/customers")
+          .withColumn( "__created_time", current_timestamp())
+  )
+
+dp.create_streaming_table(name="customer_deduped", comment="customer deduped table")
+
+dp.create_auto_cdc_flow(
+  target="customer_deduped",  
+  source="raw_customers",  
+  keys=["cust_key"], 
+  sequence_by=col("__created_time")
+)
+
+
+dp.create_streaming_table("raw_orders", comment="Incremental load")
+
+@dp.append_flow(
+  target = "raw_orders"
+)
+def raw_orders():
+  return (
+      spark.readStream
+          .format("cloudFiles")
+          .option("cloudFiles.format", "parquet")
+          .option("cloudFiles.inferColumnTypes", "true")
+          .load(f"{volumn_path}/orders")
+          .withColumn( "__created_time", current_timestamp())
+  )
+
+
+
 @dp.table(comment="customer bronze table")
-def customers_bronze():
-    return spark.read.table("kaninipro_catalog.etl.customer_raw").withColumn( "__insert_date", current_timestamp())
-            
-
-# source driver table
-@dp.table(comment="orders bronze table")
-def orders_bronze():
-    return spark.readStream.table("kaninipro_catalog.etl.orders_raw").withColumn( "__insert_date", current_timestamp())
-                
-
-# final joined table
-@dp.table(comment="joined orders and customers data")
 def joined_table():
-    return spark.sql("""select 
-                            o_custkey as cust_key, 
-                            date_format(o_orderdate, 'yyyy-MM') as year_month,
-                            case 
-                                when o_orderpriority = '1-URGENT' then 1
-                                when o_orderpriority = '2-HIGH' then 2
-                                when o_orderpriority = '3-MEDIUM' then 3
-                                when o_orderpriority = '4-NOT SPECIFIED' then 4
-                                when o_orderpriority = '5-LOW' then 5
-                                end as orderpriority,
-                            o_orderstatus as order_status,
-                            c_mktsegment as market_segment,
-                            o_totalprice as total_price
-                        from Live.orders_bronze A  
-                        join Live.customers_bronze B 
-                        on o_custkey = c_custkey
-                            """)\
-                 .withColumn( "__insert_date", current_timestamp())
+    df_o = spark.readStream.table("LIVE.raw_orders")
+    df_c = spark.read.table("LIVE.customer_deduped")
     
+    joined_df = df_o.join(df_c, df_o.o_custkey == df_c.c_custkey)\
+                  .selectExpr("c_custkey as cust_key",
+                              "o_orderdate as order_date",
+                              "o_orderkey as order_key",
+                              "o_orderpriority as order_priority",
+                              "o_orderstatus as order_status",
+                              "o_totalprice as total_price"
+                              )\
+                  .withColumn( "__created_time", current_timestamp())        
+    return joined_df
 
+
+
+dp.create_streaming_table(name="final_dataset", comment="fully joined table")
+
+dp.create_auto_cdc_flow(
+  target="final_dataset",  
+  source="joined_table",  
+  keys=["cust_key","order_key"], 
+  sequence_by=col("__created_time"),
+  stored_as_scd_type="2"
+)
