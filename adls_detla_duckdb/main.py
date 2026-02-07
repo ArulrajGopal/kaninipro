cd89d8d8db0928376528f17126f0d6de66c959a6
arulrajgopal <arulrajgopal@oulook.com>
2026-02-07 22:05:44 +0530

    update

--- adls_detla_duckdb/main.py
+++
@@ -1,6 +0,0 @@
-def main():
-    print("Hello from adls-detla-duckdb!")
-
-
-if __name__ == "__main__":
-    main()
--- adls_detla_duckdb/read_display.py
+++ adls_detla_duckdb/read_display.py
@@ -1,18 +1,42 @@
 import duckdb
+import os 
 
 con = duckdb.connect()
 con.execute("INSTALL delta;")
 con.execute("LOAD delta;")
 
-query_1 = """
+con.execute("INSTALL azure;")
+con.execute("LOAD azure;")
+
+tenant_id = os.getenv("AZ_TENANT_ID")
+client_id = os.getenv("AZ_CLIENT_ID")
+client_secret = os.getenv("AZ_CLIENT_SECRET")
+storage_ac_name = os.getenv("AZ_STORAGE_AC_NAME")
+
+
+con.execute(f"""
+        CREATE SECRET azure_spn (
+            TYPE azure,
+            PROVIDER service_principal,
+            TENANT_ID '{tenant_id}',
+            CLIENT_ID '{client_id}',
+            CLIENT_SECRET '{client_secret}',
+            ACCOUNT_NAME '{storage_ac_name}'
+);
+""")
+
+query = """
     SELECT *
-    FROM delta_scan(
-        'C:\\Users\\Arulraj Gopal\\OneDrive\\Desktop\\people_delta'
-    )
-    WHERE first_name <> 'first_name'
+    FROM delta_scan('abfss://data@kaninipro.dfs.core.windows.net/target/people_delta/')
+    where first_name <> 'first_name'
     LIMIT 10
 """
 
-df = con.execute(query_1).df()
+df = con.execute(query).df()
 print(df)
+
+
+
+
+
 
