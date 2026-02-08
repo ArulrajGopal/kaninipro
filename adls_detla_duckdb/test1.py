7383f22921dae264dcff60e0daf44123f3d4f985
arulrajgopal <arulrajgopal@oulook.com>
2026-02-08 10:44:30 +0530

    update

--- adls_detla_duckdb/test1.py
+++ adls_detla_duckdb/test1.py
@@ -25,12 +25,28 @@ con.execute(f"""
 );
 """)
 
-query = """
+count_query = """
     select count(*) from delta_scan('abfss://data@kaniniproraw.dfs.core.windows.net/test_data/sensor_data/')
 """
 
-df = con.execute(query).df()
+agg_query = """
+    select 
+    sum(sensor_a) as sum_sen_a,
+    sum(sensor_b) as sum_sen_b,
+    sum(sensor_c) as sum_sen_c,
+    sum(sensor_d) as sum_sen_d,
+    sum(sensor_e) as sum_sen_e,
+    sum(sensor_f) as sum_sen_f,
+    sum(sensor_g) as sum_sen_g,
+    sum(sensor_h) as sum_sen_h
+    from delta_scan('abfss://data@kaniniproraw.dfs.core.windows.net/test_data/sensor_data/')
+    group by unit_id
+"""
+
+df = con.execute(agg_query).df()
 print(df)
+
+# total count 6,250,000,000
 
 
 
