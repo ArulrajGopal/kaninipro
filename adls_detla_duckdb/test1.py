56118b48aec79a1a923e4249aed7f3448d883cea
arulrajgopal <arulrajgopal@oulook.com>
2026-02-08 10:41:48 +0530

    update

---
+++ adls_detla_duckdb/test1.py
@@ -0,0 +1,39 @@
+import duckdb
+import os 
+
+con = duckdb.connect()
+con.execute("INSTALL delta;")
+con.execute("LOAD delta;")
+
+con.execute("INSTALL azure;")
+con.execute("LOAD azure;")
+
+tenant_id = os.getenv("AZ_TENANT_ID")
+client_id = os.getenv("AZ_CLIENT_ID")
+client_secret = os.getenv("AZ_CLIENT_SECRET")
+storage_ac_name = os.getenv("AZ_STORAGE_AC_NAME")
+
+
+con.execute(f"""
+        CREATE SECRET azure_spn (
+            TYPE azure,
+            PROVIDER service_principal,
+            TENANT_ID '{tenant_id}',
+            CLIENT_ID '{client_id}',
+            CLIENT_SECRET '{client_secret}',
+            ACCOUNT_NAME '{storage_ac_name}'
+);
+""")
+
+query = """
+    select count(*) from delta_scan('abfss://data@kaniniproraw.dfs.core.windows.net/test_data/sensor_data/')
+"""
+
+df = con.execute(query).df()
+print(df)
+
+
+
+
+
+
