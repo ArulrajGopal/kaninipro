7640ce01109930ec894ea2465d17a66da419f9d8
arulrajgopal <arulrajgopal@oulook.com>
2026-02-08 23:18:51 +0530

    update

--- adls_detla_duckdb/src/read_trans_write_delta_heavy.py
+++ adls_detla_duckdb/src/read_trans_write_delta_heavy.py
@@ -26,45 +26,60 @@ con.execute(f"""
 );
 """)
 
-count_query = """
-    select count(*) from delta_scan('abfss://data@kaniniproraw.dfs.core.windows.net/test_data/sensor_data/')
-"""
-
-distinct_unit_id_count = """
-    select count(*) from (select 
-    distinct unit_id
-    from delta_scan('abfss://data@kaniniproraw.dfs.core.windows.net/test_data/sensor_data/')
-    )
-"""
-
-agg_query = """
-    select 
-    sum(sensor_a) as sum_sen_a,
-    sum(sensor_b) as sum_sen_b,
-    sum(sensor_c) as sum_sen_c,
-    sum(sensor_d) as sum_sen_d,
-    sum(sensor_e) as sum_sen_e,
-    sum(sensor_f) as sum_sen_f,
-    sum(sensor_g) as sum_sen_g,
-    sum(sensor_h) as sum_sen_h
-    from delta_scan('abfss://data@kaniniproraw.dfs.core.windows.net/test_data/sensor_data/')
-    group by unit_id
-"""
-
+# agg_query = """
+#     select 
+#     sum(sensor_a) as sum_sen_a,
+#     sum(sensor_b) as sum_sen_b,
+#     sum(sensor_c) as sum_sen_c,
+#     sum(sensor_d) as sum_sen_d,
+#     sum(sensor_e) as sum_sen_e,
+#     sum(sensor_f) as sum_sen_f,
+#     sum(sensor_g) as sum_sen_g,
+#     sum(sensor_h) as sum_sen_h
+#     from delta_scan('abfss://data@kaniniproraw.dfs.core.windows.net/test_data/sensor_data/')
+#     group by unit_id
+# """
+# con.execute("""
+#     COPY (
+#         SELECT *
+#         FROM delta_scan(
+#             'abfss://data@kaniniproraw.dfs.core.windows.net/people_delta/'
+#         )
+#         WHERE first_name <> 'first_name'
+#           AND id IN (3, 4)
+#     )
+#     TO 'abfss://data@kaniniproraw.dfs.core.windows.net/people_delta_filtered/people.parquet'
+#     (FORMAT parquet)
+# """)
 
 start_time = datetime.now()
-print("start_time: ", start_time)
-df = con.execute(distinct_unit_id_count).df()
-print(df)
+print("started at ", start_time)
+
+con.execute("""
+    COPY (
+        SELECT 
+        unit_id,
+        sum(sensor_a) as sum_sen_a,
+        sum(sensor_b) as sum_sen_b,
+        sum(sensor_c) as sum_sen_c,
+        sum(sensor_d) as sum_sen_d,
+        sum(sensor_e) as sum_sen_e,
+        sum(sensor_f) as sum_sen_f,
+        sum(sensor_g) as sum_sen_g,
+        sum(sensor_h) as sum_sen_h
+        FROM parquet_scan('abfss://data@kaniniproraw.dfs.core.windows.net/test_data/sensor_data/loaded_dt=20260207201903/*.parquet')
+        group by unit_id
+    )
+    TO 'abfss://data@kaniniproraw.dfs.core.windows.net/sensor_data_output/sens_parquet.parquet'
+    (FORMAT parquet)
+""")
+
+
+
 end_time = datetime.now()
 print("end_time: ", start_time)
-
-
 print("Time taken:", end_time - start_time)
 print("Successfully completed")
-
-
-# total count 6,250,000,000
 
 
 
